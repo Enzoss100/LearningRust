56d257948e3859fd0bf70d9ea5ba9301cad36a1f
Enzoss100 <lorenzossantillan@gmail.com>
2024-02-25 00:40:44 +0800

    Added Comments and new tests

--- startRust/src/main.rs
+++ startRust/src/main.rs
@@ -1,7 +1,20 @@
 fn main() {
+    // Hello World, the traditional first print of any programming language
     println!("Hello, world!");
 
+    // Testing String Variable
     let mut newVar = "Testing Strings";
 
-    println!("{newVar}")
+    // Print String Variable
+    println!("{newVar}");
+
+    // Initializing 2 integer variables
+    let newNum1: i32 = 1;
+    let newNum2: i32 = 5;
+
+    // Testing Conditional Statements
+    if newNum1 < 5 {
+        println!("{newNum2}");
+
+    }
 }
