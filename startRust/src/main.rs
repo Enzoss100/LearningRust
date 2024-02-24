63f4da84198b4c989735bd84aa99cb123e16237a
Enzoss100 <lorenzossantillan@gmail.com>
2024-02-25 01:21:20 +0800

    Fixed Errors and added non-snake-case function

--- startRust/src/main.rs
+++ startRust/src/main.rs
@@ -1,3 +1,5 @@
+#[allow(non_snake_case)]
+
 fn main() {
     // Hello World, the traditional first print of any programming language
     println!("Hello, world!");
@@ -6,15 +8,15 @@ fn main() {
     let mut new_Var = "Testing Strings";
 
     // Print String Variable
-    println!("{newVar}");
+    println!("{new_Var}");
 
     // Initializing 2 integer variables
     let newNum_1: i32 = 1;
     let newNum_2: i32 = 5;
 
     // Testing Conditional Statements
-    if newNum1 < 5 {
-        println!("{newNum2}");
+    if newNum_1 < 5 {
+        println!("{newNum_2}");
 
     }
 }
