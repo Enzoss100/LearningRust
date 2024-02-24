eff0a33a7a6a2751fd6318e7ad6c9ea6b7c7d9d0
Enzoss100 <lorenzossantillan@gmail.com>
2024-02-25 01:16:49 +0800

    Changed Variables to Snake Case

--- startRust/src/main.rs
+++ startRust/src/main.rs
@@ -3,14 +3,14 @@ fn main() {
     println!("Hello, world!");
 
     // Testing String Variable
-    let mut newVar = "Testing Strings";
+    let mut new_Var = "Testing Strings";
 
     // Print String Variable
     println!("{newVar}");
 
     // Initializing 2 integer variables
-    let newNum1: i32 = 1;
-    let newNum2: i32 = 5;
+    let newNum_1: i32 = 1;
+    let newNum_2: i32 = 5;
 
     // Testing Conditional Statements
     if newNum1 < 5 {
