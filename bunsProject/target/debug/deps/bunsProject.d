0238bd70262d312f9294d5343aaa557fbbaa9b2c
Enzoss100 <110381940+Enzoss100@users.noreply.github.com>
2024-02-29 23:19:19 +0800

    Delete bunsProject/target directory

--- bunsProject/target/debug/deps/bunsProject.d
+++
@@ -1,5 +0,0 @@
-D:\RustCodeProjects\LearningRust\bunsproject\target\debug\deps\bunsProject.exe: src\main.rs
-
-D:\RustCodeProjects\LearningRust\bunsproject\target\debug\deps\bunsProject.d: src\main.rs
-
-src\main.rs:
