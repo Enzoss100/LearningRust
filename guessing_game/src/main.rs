8ff7f23ea69154361543a804cf33de2bd04ac1bf
Enzoss100 <lorenzossantillan@gmail.com>
2024-02-29 16:59:29 +0800

    Added new Dependencies to Guessing Game Project

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use rand::Rng; //using the rand crate added in cargo.toml
 
 fn main() {
     println!("Guess the number!");
+
+    //additions after building project with rand crate
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The Secret Number is {secret_number}");
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
---
+++ guessing_game/target/debug/deps/getrandom-fc3264852fdbbfd2.d
@@ -0,0 +1,11 @@
+D:\RustCodeProjects\LearningRust\guessing_game\target\debug\deps\libgetrandom-fc3264852fdbbfd2.rmeta: C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\lib.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\error.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\util.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\error_impls.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\windows.rs
+
+D:\RustCodeProjects\LearningRust\guessing_game\target\debug\deps\libgetrandom-fc3264852fdbbfd2.rlib: C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\lib.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\error.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\util.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\error_impls.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\windows.rs
+
+D:\RustCodeProjects\LearningRust\guessing_game\target\debug\deps\getrandom-fc3264852fdbbfd2.d: C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\lib.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\error.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\util.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\error_impls.rs C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\windows.rs
+
+C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\lib.rs:
+C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\error.rs:
+C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\util.rs:
+C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\error_impls.rs:
+C:\Users\Windows\.cargo\registry\src\index.crates.io-6f17d22bba15001f\getrandom-0.2.12\src\windows.rs:
