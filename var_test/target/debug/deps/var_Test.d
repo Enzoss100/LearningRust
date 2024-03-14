a90dea8ec7de13a8550ced78e2cf117706e8d252
Enzoss100 <110381940+Enzoss100@users.noreply.github.com>
2024-03-15 00:35:52 +0800

    Delete var_test/target directory

--- var_test/target/debug/deps/var_Test.d
+++
@@ -1,5 +0,0 @@
-D:\RustCodeProjects\LearningRust\var_test\target\debug\deps\var_test.exe: src\main.rs
-
-D:\RustCodeProjects\LearningRust\var_test\target\debug\deps\var_test.d: src\main.rs
-
-src\main.rs:
--- var_test/target/debug/var_Test.d
+++
@@ -1,1 +0,0 @@
-D:\RustCodeProjects\LearningRust\var_test\target\debug\var_Test.exe: D:\RustCodeProjects\LearningRust\var_test\src\main.rs
