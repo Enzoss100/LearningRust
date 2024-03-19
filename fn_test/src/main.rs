4d985675108c855db4021bfd69dceac67be397dc
Enzoss100 <lorenzossantillan@gmail.com>
2024-03-19 20:57:14 +0800

    New Functions added

--- fn_test/src/main.rs
+++ fn_test/src/main.rs
@@ -5,11 +5,12 @@ fn main() {
 }
 
 
-//this new_func is a new function that prints a string
+// this new_func is a new function that prints a string
 fn new_func(){
     println!("This is a new function");
 }
 
+// this function has 2 parameters looking for i32 integers to be printed out
 fn param_func(x: i32, y: i32){
     println!("{x} is the first paramenter passed as an argument in this function");
     println!("{y} is the second parameter passed to this function");
