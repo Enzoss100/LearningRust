396afa415379e91823f3af857f144772ff9f829d
Enzoss100 <lorenzossantillan@gmail.com>
2024-03-20 17:26:50 +0800

    Added More Functions

--- fn_test/src/main.rs
+++ fn_test/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     // call the new_func function
     new_func();
     param_func(10,20);
+    println!("{} is a return value",testint());
 }
 
 
@@ -14,4 +15,8 @@ fn new_func(){
 fn param_func(x: i32, y: i32){
     println!("{x} is the first paramenter passed as an argument in this function");
     println!("{y} is the second parameter passed to this function");
+}
+
+fn testint() -> i32 { // this function expects an i32 data type as a return value
+    return 100; // this returns the value 100 to the compiler
 }
