311150103b7f834d0762195e36e96ee779bd95da
Enzoss100 <lorenzossantillan@gmail.com>
2024-03-22 00:05:02 +0800

    Added Control Flow Functions

--- fn_test/src/main.rs
+++ fn_test/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
     new_func();
     param_func(10,20);
     println!("{} is a return value",testint());
+    conflow1(15);
+    conflow2();
 }
 
 
@@ -19,4 +21,34 @@ fn param_func(x: i32, y: i32){
 
 fn testint() -> i32 { // this function expects an i32 data type as a return value
     return 100; // this returns the value 100 to the compiler
+}
+
+fn conflow1(y: i32) {
+    if y > 10 {
+        println!("{y} is greater than 10");
+    }
+    else{
+        println!("{y} is less than 10");
+    }
+}
+
+fn conflow2() {
+    let con = true;
+    /* 
+        here we can see that conditional statements can be used in let statements to initialize variables
+        differently
+
+        there are no semicolons in at the end of '12' and '19' because they are both expressions, not
+        statements
+
+        there is a semicolon at the end of the else statement to encapsulate the entire initialization of
+        z into one singular statement
+    */
+    let z = if con {
+                12
+            }
+            else {
+                19
+            };
+    println!("{z} is the value of the variable 'z'");
 }
