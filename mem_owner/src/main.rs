5afd190765a846d6478d0a2a0ac396d85f4bc5f6
Enzoss100 <lorenzossantillan@gmail.com>
2024-03-25 17:35:11 +0800

    Started Chapter 4

--- mem_owner/src/main.rs
+++ mem_owner/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut new_string = String::from("hello");
+    /* 
+        this String initializes the word "hello", using to dynamically allocate the string value to
+        heap memory
+    */
+    new_string.push_str(" world!"); // appends a literal to a String initialized variable
+    println!("{}", new_string);
 }
