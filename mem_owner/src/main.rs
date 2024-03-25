3369ec7ed72f67be76a1ce245915548b2c651dbb
Enzoss100 <lorenzossantillan@gmail.com>
2024-03-26 00:31:47 +0800

    Added more detailed comments

--- mem_owner/src/main.rs
+++ mem_owner/src/main.rs
@@ -18,10 +18,35 @@ fn main() {
         is being borrowed from a place where data has been moved already
 
         Ex:
-         
+
         let string1 = String::from("Hello");
         let string2 = string1;
     
     */
     println!("{}, {} World!", string1, string2);
+    println!();
+    println!();
+
+    let string3 = String::from("Memory"); 
+    // Here string3 owns the memory allocated for the String initialized
+    let (string4, len) = calc_len(string3);
+    /* 
+        This tuple of string4 and len are initialized to the function of 
+        calc_len that takes the value of string3
+    */
+    println!("The length of {} is {}", string4, len); //prints the values in the tuple individually
+}
+
+fn calc_len(s: String) -> (String, usize){ 
+    // the function expects a String and will return a String and usize
+    let length = s.len(); // returns the length of the string obtained by s
+    return (s, length) // returns the values of s and length which are String and usize in data type
+
+    /*
+        In terms of ownership, the value of string3 is passed to the function
+        The function now having ownership of the value can now deal with it as needed
+
+        The function checks through the pointer of string3, and uses the len() function to check 
+        the length of the string given in the heap memory (not the capacity)
+    */
 }
